4ef31f1c7d55c1c85e92e414888463f3c27e5c07
Jose Zafrilla Ruiz <jozaru27@gmail.com>
2025-01-24 10:05:27 +0100

    Update
    Fixed and limited Big_Blinky's enemy path on Navmesh
    Added consumables that are destroyed on use which destroy all instantiated enemies

--- Assets/_ChompMan-PM/Scripts/NavJugador.cs
+++ Assets/_ChompMan-PM/Scripts/NavJugador.cs
@@ -7,15 +7,18 @@ public class NavJugador : MonoBehaviour
 {
     float velocidad;
     GameObject GameobjectwithCharacterController;
-    CharacterController controller ;
+    CharacterController controller;
+
     void Start()
     {
         controller = this.GetComponent<CharacterController>();
          velocidad = 3f;
     }
+
     void Update()
     {
     }
+
     void FixedUpdate()
     {
 
@@ -30,11 +33,35 @@ public class NavJugador : MonoBehaviour
         //Genero el vector de movimiento
         //Muevo el jugador
         //transform.position += anguloTeclas * velocidad * Time.deltaTime;
+
         controller.Move(anguloTeclas * velocidad * Time.deltaTime);
         if (anguloTeclas != null && anguloTeclas != Vector3.zero)
         {
             transform.forward = anguloTeclas * 1;
             transform.rotation = Quaternion.LookRotation(anguloTeclas);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // Detecta colisión con el objeto Cherry
+        if (other.CompareTag("Cherry"))
+        {
+            // Encuentra los enemigos por su nombre y los desactiva
+            GameObject blinky = GameObject.Find("Blinky");
+            GameObject bigBlinky = GameObject.Find("Big_Blinky");
+
+            if (blinky != null)
+            {
+                blinky.SetActive(false);
+            }
+
+            if (bigBlinky != null)
+            {
+                bigBlinky.SetActive(false);
+            }
+
+            // Destruye o desactiva la Cherry
+            Destroy(other.gameObject);
         }
     }
 
